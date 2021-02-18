71be0ba22ecaa459482ae321e7cc771b90014ea5
Imaad Uddin <imaaduddin@icloud.com>
2021-02-17 23:44:36 -0600

    finished madlib phrase

--- madlibs.py
+++ madlibs.py
@@ -3,4 +3,6 @@ verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 anime_character = input("Anime character: ")
 
-madlib = f""
+madlib = f"Running through the village is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay amazing and {verb2} like you are {anime_character}!"
+
+print(madlib)
